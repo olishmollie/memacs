928d695a80d50fc459bcdea0b2b4a3e9290bd034
ajbond <olishmollie@gmail.com>
2019-12-28 10:36:28 -0600

    Add custom.el to repo
    I might get rid of this file later, I'm not wild about Custom.

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (evil-surround evil-commentary which-key vterm use-package shell-pop projectile monokai-theme magit lsp-ui lsp-ivy flycheck exec-path-from-shell evil diff-hl company-lsp auto-package-update))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
