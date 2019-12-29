118d18665a7dca1b6a8c2fa20fa36c3671496648
ajbond <olishmollie@gmail.com>
2019-12-28 21:18:35 -0600

    Define default key bindings

--- bindings.el
+++ bindings.el
@@ -6,25 +6,23 @@
 ;;; Code:
 (require 'core "~/.emacs.d/core.el")
 
-(memacs/make-prefix-map 'memacs-prefix-map memacs-prefix-key
-			evil-motion-state-map "MeMacs")
+(memacs/make-prefix-map 'memacs-prefix-map memacs-prefix-key evil-motion-state-map)
+
+(memacs/bind-prefix-map 'memacs-prefix-map memacs-prefix-bindings)
 
 ;; Magit
 (memacs/make-prefix-map 'memacs-magit-prefix-map
 			memacs-magit-prefix-key
 			'memacs-prefix-map "Magit")
 
-(memacs/bind-prefix-map 'memacs-magit-prefix-map '(("s" . magit-status)))
+(memacs/bind-prefix-map 'memacs-magit-prefix-map memacs-magit-prefix-bindings)
 
 ;; Buffer Management
 (memacs/make-prefix-map 'memacs-buffer-prefix-map
 			memacs-buffer-prefix-key
 			'memacs-prefix-map "Buffers")
 
-(memacs/bind-prefix-map 'memacs-buffer-prefix-map '(("b" . list-buffers)
-						    ("p" . previous-buffer)
-						    ("n" . next-buffer)
-						    ("d" . kill-this-buffer)))
+(memacs/bind-prefix-map 'memacs-buffer-prefix-map memacs-buffer-prefix-bindings)
 
 ;; For some reason, Buffer-menu-mode-map is listed in evil-overriding-maps.
 ;; This clobbers the SPC prefix, so we need to override it here.
@@ -35,38 +33,22 @@
 			memacs-window-prefix-key
 			'memacs-prefix-map "Windows")
 
-(memacs/bind-prefix-map 'memacs-window-prefix-map
-			'(("/" . ("vertical-split" . evil-window-vsplit))
-			  ("-" . ("horizontal-split" . evil-window-split))
-			  ("h" . ("window-left" . evil-window-left))
-			  ("l" . ("window-right" . evil-window-right))
-			  ("k" . ("window-up" . evil-window-up))
-			  ("j" . ("window-down" . evil-window-down))
-			  ("d" . ("window-delete" . evil-window-delete))
-			  ("o" . delete-other-windows)))
+(memacs/bind-prefix-map 'memacs-window-prefix-map memacs-window-prefix-bindings)
 
 ;; File Management
 (memacs/make-prefix-map 'memacs-file-prefix-map
 			memacs-file-prefix-key
 			'memacs-prefix-map "Files")
 
-(memacs/bind-prefix-map 'memacs-file-prefix-map '(("f" . find-file)))
+(memacs/bind-prefix-map 'memacs-file-prefix-map memacs-file-prefix-bindings)
 
 ;; Help
 (memacs/make-prefix-map 'memacs-help-prefix-map
 			memacs-help-prefix-key
 			'memacs-prefix-map "Help")
 
-(memacs/bind-prefix-map 'memacs-help-prefix-map '(("v" . describe-variable)
-						  ("f" . describe-function)
-						  ("m" . describe-mode)
-						  ("k" . describe-key)
-						  ("?" . help-for-help)
-						  ("." . display-local-help)
-						  ("P" . describe-package)
-						  ("a" . apropos-command)
-						  ("s" . describe-syntax)))
-  
+(memacs/bind-prefix-map 'memacs-help-prefix-map memacs-help-prefix-bindings)
+
 ;; Terminal Management
 (global-set-key (kbd "C-'") 'shell-pop)
 
--- core.el
+++ core.el
@@ -4,11 +4,9 @@
 
 ;;; Code:
 (load-file "~/.emacs.d/packages.el")
+
 (require 'evil)
 (require 'which-key)
-
-;; This enables which-key to process cons arguments of `define-key'.
-(setq which-key-enable-extended-define-key t)
 
 ;; Defaults
 (defvar memacs-prefix-key "SPC"
@@ -24,6 +22,48 @@
 (defvar memacs-help-prefix-key "h"
   "Help access prefix key.  Defaults to 'h'.")
 
+(defvar memacs-prefix-bindings
+  '(("SPC" . ("command" . execute-extended-command))
+    (":" . ("eval" . eval-expression)))
+  "Default top level MeMacs prefix bindings.")
+
+(defvar memacs-magit-prefix-bindings '(("s" . magit-status))
+  "Default magit prefix bindings.")
+
+(defvar memacs-buffer-prefix-bindings
+  '(("b" . list-buffers)
+    ("p" . previous-buffer)
+    ("n" . next-buffer)
+    ("d" . kill-this-buffer))
+  "Default buffer prefix bindings.")
+
+(defvar memacs-window-prefix-bindings
+  '(("/" . ("vertical-split" . evil-window-vsplit))
+    ("-" . ("horizontal-split" . evil-window-split))
+    ("h" . ("window-left" . evil-window-left))
+    ("l" . ("window-right" . evil-window-right))
+    ("k" . ("window-up" . evil-window-up))
+    ("j" . ("window-down" . evil-window-down))
+    ("d" . ("window-delete" . evil-window-delete))
+    ("o" . delete-other-windows))
+  "Default window prefix bindings.")
+
+(defvar memacs-file-prefix-bindings
+  '(("f" . find-file))
+  "Default file prefix bindings.")
+
+(defvar memacs-help-prefix-bindings
+ '(("v" . describe-variable)
+   ("f" . describe-function)
+   ("m" . describe-mode)
+   ("k" . describe-key)
+   ("?" . help-for-help)
+   ("." . display-local-help)
+   ("P" . describe-package)
+   ("a" . apropos-command)
+   ("s" . describe-syntax))
+ "Default help prefix bindings.")
+ 
 (defun memacs/make-prefix-map (symbol key parent &optional name)
   "Bind SYMBOL to a new prefix given by KEY with parent keymap PARENT.
 
@@ -36,7 +76,9 @@
 (defun memacs/bind-prefix-map (map bindings)
   "Bind BINDINGS to MAP."
   (dolist (binding bindings)
-    (define-key map (car binding) (cdr binding))))
+    (let ((key (car binding))
+	  (def (cdr binding)))
+      (define-key map (kbd key) def))))
 
 (provide 'core)
 ;;; core ends here
