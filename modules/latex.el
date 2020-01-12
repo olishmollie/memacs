14d4cb6558749726b5e787c60c7c923db6645112
ajbond <olishmollie@gmail.com>
2020-01-12 15:35:18 -0600

    Fix latex module

--- modules/latex.el
+++ modules/latex.el
@@ -16,29 +16,35 @@
     "SPC m" "Latex"
     "SPC m e" "Environments"
     "SPC m n" "Narrowing")
+
   (general-create-definer memacs/latex-prefix
     :states '(normal insert emacs visual visual-line)
     :keymaps 'local
     :prefix "SPC m"
-    :non-normal-prefix "M-SPC m"
+    :non-normal-prefix "M-SPC m")
+  (memacs/latex-prefix
     "`" '(TeX-next-error :whick-key "Display Error")
     "a" '(TeX-command-run-all :which-key "Compile")
     "c" '(TeX-clean :which-key "Clean")
     "v" '(TeX-view :which-key "View"))
+
   (general-create-definer memacs/latex-environments-prefix
     :states '(normal insert emacs visual visual-line)
     :keymaps 'local
     :prefix "SPC m e"
-    :non-normal-prefix "M-SPC m e"
+    :non-normal-prefix "M-SPC m e")
+  (memacs/latex-environments-prefix
     "i" '(LaTeX-environment :which-key "Insert Environment")
     "c" '(LaTeX-close-environment :which-key "Close Environment")
     "b" '(LaTeX-find-matching-begin :which-key "Find Begin")
     "e" '(LaTeX-find-matching-end :which-key "Find End"))
+
   (general-create-definer memacs/latex-narrowing-prefix
     :states '(normal insert emacs visual visual-line)
     :keymaps 'local
     :prefix "SPC m n"
-    :non-normal-prefix "M-SPC m n"
+    :non-normal-prefix "M-SPC m n")
+  (memacs/latex-narrowing-prefix
     "g" '(TeX-narrow-to-group :which-key "Narrow to Group")
     "e" '(LaTeX-narrow-to-environment) :which-key "Narrow to Environment"))
 
