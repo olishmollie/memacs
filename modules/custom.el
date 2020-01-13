2365379e06374076043f6c86d2cfc059ae0b1ade
ajbond <olishmollie@gmail.com>
2020-01-12 22:34:39 -0600

    Add directory local variable to a project

--- modules/custom.el
+++ modules/custom.el
@@ -3,9 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (auctex which-key vterm use-package shell-pop projectile neotree lsp-ui lsp-ivy general flycheck exec-path-from-shell evil-surround evil-magit evil-commentary doom-themes doom-modeline diff-hl counsel company-lsp auto-package-update))))
+ '(safe-local-variable-values (quote ((memacs-enable-clang-format-on-save)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
