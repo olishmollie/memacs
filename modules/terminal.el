f08a4d9fcfb336cf332aed855483b589959c9c9e
ajbond <olishmollie@gmail.com>
2020-01-14 14:07:31 -0600

    Open a terminal in project root if possible

--- modules/terminal.el
+++ modules/terminal.el
@@ -5,12 +5,21 @@
 
 ;;; Code:
 
+;; TODO: This function requires starting a new shell in order to open at the
+;; project root. I'd like to not have to do that.
+(defun memacs/shell-pop()
+  "Open a shell in the project root if it exists."
+  (interactive)
+  (let ((default-directory (or (projectile-project-root) default-directory)))
+      (vterm)))
+
 (use-package vterm)
 (use-package shell-pop
   :after vterm
   :init (setq shell-pop-universal-key "M-'"
               shell-pop-full-span t
-	      shell-pop-shell-type '("vterm" "*vterm*" (lambda nil (vterm)))))
+              shell-pop-autocd-to-working-dir nil
+	      shell-pop-shell-type '("vterm" "*vterm*" #'memacs/shell-pop)))
 
 (evil-set-initial-state 'vterm-mode 'emacs)
 
