25d53899660de010ee845abcef82ca74bf3187d5
ajbond <olishmollie@gmail.com>
2020-01-24 14:03:46 -0600

    Add prettier-js to custom

--- modules/custom.el
+++ modules/custom.el
@@ -3,6 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (prettier-js which-key vterm use-package shell-pop projectile neotree lsp-ui lsp-ivy latex-preview-pane general flycheck exec-path-from-shell evil-surround evil-magit evil-commentary doom-themes doom-modeline diff-hl company-lsp auto-package-update auctex)))
  '(safe-local-variable-values (quote ((memacs-enable-clang-format-on-save)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
