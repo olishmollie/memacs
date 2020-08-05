267e1b5a76e4bb28759232a6ca587a8b446140d9
ajbond <olishmollie@gmail.com>
2020-08-05 09:35:40 -0500

    Use rust-clippy for rust syntax checking

--- modules/rust.el
+++ modules/rust.el
@@ -18,6 +18,8 @@
 
 (add-hook 'rust-mode-hook
           (lambda ()
+            (when (fboundp #'flycheck-select-checker)
+                (flycheck-select-checker 'rust-clippy))
             (add-hook 'before-save-hook #'memacs-rust-format-on-save nil t)))
 
 ;;; rust ends here
